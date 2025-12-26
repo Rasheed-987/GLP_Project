5d51ef512aa97480bc7780acf079b580a9cc72af
Abdul-RasheedMative <Abdul_Rasheed@mativeinc.com>
2025-12-26 18:48:22 +0500

    build fix

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,8 +1,35 @@
 import '../globals.css'
 import type { Locale } from "../../lib/i18n/config"
 import { getDictionary } from "../../lib/i18n/dictionaries"
-import DirectionProvider from '../components/DirectionProvider'
+import localFont from 'next/font/local'
+import { Toaster } from 'react-hot-toast'
 import Providers from './providers'
+
+const frutiger = localFont({
+  src: [
+    {
+      path: '../fonts/FrutigerLTArabic45Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/FrutigerLTArabic55Roman.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/FrutigerLTArabic65Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/frutigerltarabic75black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-frutiger-arabic',
+})
 
 export async function generateMetadata({ params }: { params: Promise<{ lang: string }> }) {
   const { lang } = await params
@@ -24,11 +51,16 @@ export default async function LangLayout({
 }) {
   const { lang } = await params
   const locale = lang as Locale
+  const dir = locale === 'ar' ? 'rtl' : 'ltr'
+  
   return (
-    <Providers>
-      <DirectionProvider lang={locale}>
-        {children}
-      </DirectionProvider>
-    </Providers>
+    <html lang={locale} dir={dir}>
+      <body className={`${frutiger.variable} font-sans antialiased`}>
+        <Toaster position="top-center" />
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
   )
 }
